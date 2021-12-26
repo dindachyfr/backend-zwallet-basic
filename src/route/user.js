21fcb2957f17d49a8e495ae12d5c2a7916fed596
dindachyfr <dindachyfr@gmail.com>
2021-12-26 13:36:10 +0700

    add linter

--- src/route/user.js
+++ src/route/user.js
@@ -1,17 +1,15 @@
-const express = require('express')
-const userController = require('../controller/user')
-const middleware = require('../middleware/midware')
+const express = require("express");
+const userController = require("../controller/user");
+const middleware = require("../middleware/midware");
 
-const route = express.Router()
+const route = express.Router();
 
 route
-  .post('/', userController.postUser)
-  .get('/', userController.getUsersFiltered)
-  .delete('/:id', userController.delUser)
-  .put('/:id', userController.updateUser)
-  .post('/register', middleware.midUser,userController.registerUser)
-  .post('/login', userController.loginUser)
+  .post("/", userController.postUser)
+  .get("/", userController.getUsersFiltered)
+  .delete("/:id", userController.delUser)
+  .put("/:id", userController.updateUser)
+  .post("/register", middleware.midUser, userController.registerUser)
+  .post("/login", userController.loginUser);
 
-
-  
-module.exports = route
+module.exports = route;
--- src/route/wallet.js
+++ src/route/wallet.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const walletController = require('../controller/wallet')
-const middleware =  require('../middleware/midware')
-const route = express.Router()
+const express = require("express");
+const walletController = require("../controller/wallet");
+const middleware = require("../middleware/midware");
+const route = express.Router();
 
 route
-  .post('/', middleware.midWallet, walletController.postWallet)
-   .get('/', walletController.getWallet)
-   .get('/:id', walletController.getDetailedWallet)
-   .put('/:id', walletController.updateWallet)
-  
-module.exports = route
+  .post("/", middleware.midWallet, walletController.postWallet)
+  .get("/", walletController.getWallet)
+  .get("/:id", walletController.getDetailedWallet)
+  .put("/:id", walletController.updateWallet);
+
+module.exports = route;

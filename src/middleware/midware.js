2f7bab36de0996386d2d82fb83e2efab4f3b465d
dindachyfr <dindachyfr@gmail.com>
2021-12-26 13:35:52 +0700

    add linter

--- src/middleware/midware.js
+++ src/middleware/midware.js
@@ -1,53 +1,50 @@
-const { use } = require('express/lib/router')
-const modelWallet = require('../models/wallet')
+/* eslint-disable no-undef */
+/* eslint-disable camelcase */
+const modelWallet = require("../models/wallet");
 
+const midTransfer = async (req, res, next) => {
+  try {
+    const { sender_wallet_id, amount } = req.body; // masuk ke middleware buat cek saldo>amount
+    const checkSenderWallet = await modelWallet.getDetailedWallet(sender_wallet_id); // masuk ke middleware buat cek saldo>amount
+    const senderBalance = checkSenderWallet[0].balance; // masuk ke middleware buat cek saldo>amount
 
-const midTransfer = async (req,res,next) =>{
-    try{
-        const {sender_wallet_id, amount} = req.body //masuk ke middleware buat cek saldo>amount
-        const checkSenderWallet = await modelWallet.getDetailedWallet(sender_wallet_id) //masuk ke middleware buat cek saldo>amount
-        const senderBalance = checkSenderWallet[0].balance //masuk ke middleware buat cek saldo>amount
-    
-        if (senderBalance > amount){next()}
-    
-        else{            
-        const errorRes = new Error('The transfer amount is exceeding your current balance')
-        errorRes.status = 500
-        next(errorRes)
-        }
-}catch (error) {
-    const errorRes = new Error('Internal Server Error')
-    errorRes.status = 500
-    console.log(error)
-    next(errorRes)
+    if (senderBalance > amount) { next(); } else {
+      const errorRes = new Error("The transfer amount is exceeding your current balance");
+      errorRes.status = 500;
+      next(errorRes);
+    }
+  } catch (error) {
+    const errorRes = new Error("Internal Server Error");
+    errorRes.status = 500;
+    console.log(error);
+    next(errorRes);
   }
+};
 
-}
+const midWallet = async (req, res, next) => {
+  try {
+    // eslint-disable-next-line camelcase
+    const { balance, user_id } = req.body;
+    if (!balance || !user_id) {
+      const errorRes = new Error("Please insert balance or user_id properly");
+      errorRes.status = 500;
+      next(errorRes);
+    } else { next(); }
+  } catch (error) {
+    const errorRes = new Error("Internal Server Error");
+    errorRes.status = 500;
+    console.log(error);
+    next(errorRes);
+  }
+};
 
-const midWallet = async (req, res, next) =>{
-    try{
-        const {balance, user_id} = req.body
-        if (!balance || !user_id){
-            const errorRes = new Error('Please insert balance or user_id properly')
-            errorRes.status = 500
-            next(errorRes)
-            }
-        else{next()}
-    }catch (error) {
-        const errorRes = new Error('Internal Server Error')
-        errorRes.status = 500
-        console.log(error)
-        next(errorRes)
-      }
-}
+midUser = (req, res, next) => {
+  const { name, phone_number, username, email, password, pin } = req.body;
+  if (!name || !phone_number || !username || !email || !password || !pin) {
+    const errorRes = new Error("Please fill in all forms!");
+    errorRes.status = 500;
+    next(errorRes);
+  } else { next(); }
+};
 
-midUser = (req, res, next)=>{
-    const {name, phone_number, username, email, password, pin} = req.body
-    if(!name || !phone_number || !username || !email || !password || !pin){
-        const errorRes = new Error('Please fill in all forms!')
-        errorRes.status = 500
-        next(errorRes)
-    }else{next()}
-}
-
-module.exports = {midTransfer, midWallet, midUser}
+module.exports = { midTransfer, midWallet, midUser };

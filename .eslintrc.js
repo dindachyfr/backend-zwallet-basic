4ae53152995c1ca6e72092a8d448f33b12e4e3bd
dindachyfr <dindachyfr@gmail.com>
2021-12-26 13:34:48 +0700

    add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,20 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es2021": true
-    },
-    "extends": [
-        "standard"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 13
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "quotes": ["double"],
-        "semi": ["always"]
-    }
-},
-{
-    "extends": "standard"
-  };
+  env: {
+    commonjs: true,
+    es2021: true
+  },
+  extends: [
+    "standard"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 13
+  },
+  plugins: [
+    "@typescript-eslint"
+  ],
+  rules: {
+    quotes: ["error", "double"],
+    semi: ["error", "always"]
+  }
+};
--- indexx.js
+++ indexx.js
@@ -1,53 +1,50 @@
-//npm  install mysql2
-//npm install express
-//npm install nodemon -D
-//npm install morgan        ==>buat mempermudah debugging
-//npm install dotenv
-//npm install bcrypt    ==>buat password hashing
+// npm  install mysql2
+// npm install express
+// npm install nodemon -D
+// npm install morgan        ==>buat mempermudah debugging
+// npm install dotenv
+// npm install bcrypt    ==>buat password hashing
 
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const userRoute = require('./src/route/user')
-const walletRoute = require('./src/route/wallet')
-const transactionRoute = require('./src/route/transaction')
-const helper =  require('./src/helper/common')
-const morgan = require('morgan')
-const mysql = require('mysql2')
-const connection = require('./src/config/db')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const userRoute = require("./src/route/user");
+const walletRoute = require("./src/route/wallet");
+const transactionRoute = require("./src/route/transaction");
+const helper = require("./src/helper/common");
+const morgan = require("morgan");
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running in port ${PORT}`);
+});
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, ()=>{
-    console.log(`Server running in port ${PORT}`)
-})
-
-//middlewear to access json data
+// middlewear to access json data
 app.use(express.json());
 
-//memermudah debugging
-app.use(morgan('dev'))
+// memermudah debugging
+app.use(morgan("dev"));
 
-//handle data user
-app.use('/users', userRoute)
+// handle data user
+app.use("/users", userRoute);
 
-//handle data wallet
-app.use('/user-wallet', walletRoute)
+// handle data wallet
+app.use("/user-wallet", walletRoute);
 
-//handle data transaction
-app.use('/transaction', transactionRoute)
+// handle data transaction
+app.use("/transaction", transactionRoute);
 
-//handle typo in writing path
-app.use(helper.handleNotFound)
+// handle typo in writing path
+app.use(helper.handleNotFound);
 
-//handle error
+// handle error
 
-app.use((err, req, res, next)=>{
-    const statusCode = err.status || 500
-    const message = err.message || 'Internal Server Error'
-    res.status(statusCode)
-    res.json({
-      status: statusCode,
-      message: message
-    })
-  })
+app.use((err, req, res) => {
+  const statusCode = err.status || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode);
+  res.json({
+    status: statusCode,
+    message: message
+  });
+});

b0ae848ef8fc424fa056441d90b684304aa981dd
dindachyfr <dindachyfr@gmail.com>
2021-12-26 13:36:01 +0700

    add linter

--- src/models/user.js
+++ src/models/user.js
@@ -1,94 +1,91 @@
-const connection = require('../config/db')
+const connection = require("../config/db");
 
-const countUsers =() =>{
-  return new Promise((resolve, reject)=>{
-    connection.query("SELECT COUNT (*) AS total FROM users", (error,result)=>{
-      if(!error){
-        resolve(result)
-      }else{
-        reject(error)
-      }
-    })
-  })
-}
-
-const getUsersFiltered = ({filter, limit, offset})=>{
-  return new Promise((resolve, reject)=>{
-    connection.query(`SELECT * FROM users WHERE name LIKE '%${filter}%' LIMIT ${limit} OFFSET ${offset}` ,(error, result) => {
+const countUsers = () => {
+  return new Promise((resolve, reject) => {
+    connection.query("SELECT COUNT (*) AS total FROM users", (error, result) => {
       if (!error) {
-        resolve(result)
+        resolve(result);
       } else {
-        reject(error)
+        reject(error);
       }
-    })
-  })
-}
+    });
+  });
+};
 
+const getUsersFiltered = ({ filter, limit, offset }) => {
+  return new Promise((resolve, reject) => {
+    connection.query(`SELECT * FROM users WHERE name LIKE '%${filter}%' LIMIT ${limit} OFFSET ${offset}`, (error, result) => {
+      if (!error) {
+        resolve(result);
+      } else {
+        reject(error);
+      }
+    });
+  });
+};
 
-const postUser = (dataUser)=>{
-    return new Promise((resolve, reject)=>{
-        connection.query("INSERT INTO users set ?", dataUser, (error, result) => {
-          if (!error) {
-            resolve(result)
-          } else {
-            reject(error)
-          }
-        })
-      })
-}
+const postUser = (dataUser) => {
+  return new Promise((resolve, reject) => {
+    connection.query("INSERT INTO users set ?", dataUser, (error, result) => {
+      if (!error) {
+        resolve(result);
+      } else {
+        reject(error);
+      }
+    });
+  });
+};
 
-const delUser = (id)=>{
-    return new Promise((resolve, reject)=>{
-        connection.query("DELETE FROM users WHERE id = ?", id, (error, result)=>{
-            if(!error){ resolve(result) }
-            else { reject(error) }
-        })
-    })
-}
+const delUser = (id) => {
+  return new Promise((resolve, reject) => {
+    connection.query("DELETE FROM users WHERE id = ?", id, (error, result) => {
+      if (!error) { resolve(result); } else { reject(error); }
+    });
+  });
+};
 
-const updateUser = (dataUser, id)=>{
-    return new Promise((resolve, reject)=>{
-      connection.query("UPDATE users SET ? WHERE id = ?", [dataUser, id], (error, result) => {
-        if (!error) {
-          resolve(result)
-        } else {
-          reject(error)
-        }
-      })
-    })
-   
-  }
+const updateUser = (dataUser, id) => {
+  return new Promise((resolve, reject) => {
+    connection.query("UPDATE users SET ? WHERE id = ?", [dataUser, id], (error, result) => {
+      if (!error) {
+        resolve(result);
+      } else {
+        reject(error);
+      }
+    });
+  });
+};
 
-  const findByEmail = (email) =>{
-    return new Promise ((resolve, reject)=>{
-      connection.query("SELECT * FROM users WHERE email = ?", email, (error, result)=>{
-        if(!error){
-          resolve(result)
-        }else{
-          reject(error)
-        }
-      })
-    })
-  }
+const findByEmail = (email) => {
+  return new Promise((resolve, reject) => {
+    connection.query("SELECT * FROM users WHERE email = ?", email, (error, result) => {
+      if (!error) {
+        resolve(result);
+      } else {
+        reject(error);
+      }
+    });
+  });
+};
 
-  const createAccount = (data) =>{
-    return new Promise ((resolve, reject)=>{
-      connection.query("INSERT INTO users SET ?", data, (error, result)=>{
-        if(!error){
-          resolve(result)
-        }else{
-          reject(error)
-        }
-      })
-    })
-  }
+const createAccount = (data) => {
+  return new Promise((resolve, reject) => {
+    connection.query("INSERT INTO users SET ?", data, (error, result) => {
+      if (!error) {
+        resolve(result);
+      } else {
+        reject(error);
+      }
+    });
+  });
+};
 
 module.exports = {
-    postUser: postUser,
-    delUser: delUser,
-    updateUser: updateUser,
-    getUsersFiltered: getUsersFiltered,
-    countUsers: countUsers,
-    findByEmail: findByEmail,
-    createAccount: createAccount,
-}
+  postUser: postUser,
+  delUser: delUser,
+  updateUser: updateUser,
+  getUsersFiltered: getUsersFiltered,
+  countUsers: countUsers,
+  findByEmail: findByEmail,
+  createAccount: createAccount
+};

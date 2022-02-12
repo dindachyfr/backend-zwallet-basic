1a4f7aea370bbf4de512ecd30294bb4d11c0add7
dindachyfr <dindachyfr@gmail.com>
2022-02-12 14:55:00 -0800

    add pool

--- src/config/db.js
+++ src/config/db.js
@@ -7,17 +7,17 @@ const connection = mysql.createConnection({
   database: process.env.DB_NAME
 });
 
-const pool = mysql.createPool({
-  connectionLimit: 10,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
+// const pool = mysql.createPool({
+//   connectionLimit: 10,
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME
+// });
 
-pool.query("SELECT 1 + 1 AS solution", function (error, results, fields) {
-  if (error) throw error;
-  console.log("The solution is: ", results[0].solution);
-});
+// pool.query("SELECT 1 + 1 AS solution", function (error, results, fields) {
+//   if (error) throw error;
+//   console.log("The solution is: ", results[0].solution);
+// });
 
 module.exports = connection;

dd7f258b362190dc4f15b45337067ddc3548757d
dindachyfr <dindachyfr@gmail.com>
2022-02-12 14:51:45 -0800

    add pool

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,13 @@
 const mysql = require("mysql2");
 
-// const connection = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME
-// });
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
 
-const connection = mysql.createPool({
+const pool = mysql.createPool({
   connectionLimit: 10,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -15,7 +15,7 @@ const connection = mysql.createPool({
   database: process.env.DB_NAME
 });
 
-connection.query("SELECT 1 + 1 AS solution", function (error, results, fields) {
+pool.query("SELECT 1 + 1 AS solution", function (error, results, fields) {
   if (error) throw error;
   console.log("The solution is: ", results[0].solution);
 });
